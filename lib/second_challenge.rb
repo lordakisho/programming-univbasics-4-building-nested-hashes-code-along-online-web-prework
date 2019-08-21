1e615b98c091d0091da3bc718c8961f174781385
Eddie Parker <eddie.parker88@gmail.com>
2019-08-21 17:53:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch
+      :patriarch => {}
       :matriarch
       :heroine
       :heroine_friends
